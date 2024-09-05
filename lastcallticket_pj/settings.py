95356ec5f81729d150be04204b719c2d94d5b79c
GabyRosas <fleuryrosas@gmail.com>
2024-09-06 01:03:12 +0200

    configuration connection BBDD ok + create superuser

--- lastcallticket_pj/settings.py
+++ lastcallticket_pj/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lastcallticket_pj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lastcallticket',
+        'USER': 'postgres',
+        'PASSWORD': 'rootF5',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
