aedf4f8411bc79295b65bc1dec1c903c4a72d239
GabyRosas <fleuryrosas@gmail.com>
2024-09-10 00:44:52 +0200

    add update serializer for tickets

--- lastcallticket_pj/settings.py
+++ lastcallticket_pj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'user_app',
     'rest_framework.authtoken',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,6 +131,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    '*'
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
--- ticket_app/serializer.py
+++ ticket_app/serializer.py
@@ -9,7 +9,34 @@ class TransportSerializer(serializers.ModelSerializer):
 
 class TicketSerializer(serializers.ModelSerializer):
     transport = TransportSerializer()
+    username = serializers.SerializerMethodField()
     class Meta:
         model = Ticket
         fields = '__all__'
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def update(self, instance, validated_data):
+        # Actualiza los campos del Ticket
+        instance.departure_place = validated_data.get('departure_place', instance.departure_place)
+        instance.arrival_place = validated_data.get('arrival_place', instance.arrival_place)
+        instance.departure_date = validated_data.get('departure_date', instance.departure_date)
+        instance.return_date = validated_data.get('return_date', instance.return_date)
+        instance.original_price = validated_data.get('original_price', instance.original_price)
+        instance.proposed_price = validated_data.get('proposed_price', instance.proposed_price)
+        instance.note = validated_data.get('note', instance.note)
+
+        validated_data.pop('user', None)
+
+        # Actualiza el campo transport si está presente en los datos validados
+        transport_data = validated_data.get('transport')
+        if transport_data:
+            transport_instance = instance.transport
+            transport_instance.transport_name = transport_data.get('transport_name', transport_instance.transport_name)
+            transport_instance.transport_type = transport_data.get('transport_type', transport_instance.transport_type)
+            transport_instance.save()
+
+        instance.save()
+        return instance
 
